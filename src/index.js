88dd0e4b09677b552678763a827693a037762075
Oleg Kravchuk <oleg.kravchuk@gospime.com>
2019-11-12 14:38:22 +0200

    fix: refactored

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 const bcrypt = require('bcryptjs');
 
-const Crypt = function () {};
+const Crypt = class {
+  static async hash(value, strength) {
+    return new Promise(function (resolve, reject) {
+      const callback = function (error, hash) {
+        if (error) reject(error);
+        else resolve(hash);
+      };
 
-Crypt.hash = function (value, strength) {
-  return new Promise(function (resolve, reject) {
-    const callback = function (error, hash) {
-      if (error) reject(error);
-      else resolve(hash);
-    };
+      bcrypt.hash(value, strength || 10, callback);
+    });
+  }
 
-    bcrypt.hash(value, strength || 10, callback);
-  });
-};
+  static async compare(value, hash) {
+    return new Promise(function (resolve, reject) {
+      const callback = function (error, isEqual) {
+        if (error) reject(error);
+        else resolve(isEqual);
+      };
 
-Crypt.compare = function (value, hash) {
-  return new Promise(function (resolve, reject) {
-    const callback = function (error, isEqual) {
-      if (error) reject(error);
-      else resolve(isEqual);
-    };
-
-    bcrypt.compare(value, hash, callback);
-  });
+      bcrypt.compare(value, hash, callback);
+    });
+  }
 };
 
 module.exports = Crypt;
